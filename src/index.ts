200c1b3ad2e3392cc5aa1354ad01c708f9e1b2e4
Alberto Benatti <albi.benni8@gmail.com>
2026-01-19 09:47:05 +0100

    testing embedding

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import { openai } from "@ai-sdk/openai";
 import { cosineSimilarity, embedMany } from "ai";
 
 async function main() {
-    const { embeddings } = await embedMany({
-        model: "openai/text-embedding-3-small",
-        values: ["sunny day at the beach", "rainy afternoon in the city"],
-    });
+  const { embeddings, usage } = await embedMany({
+    model: "openai/text-embedding-3-small",
+    values: ["sunny day at the beach", "rainy afternoon in the city"],
+  });
 
-    console.log(
-        `cosine similarity: ${cosineSimilarity(embeddings[0], embeddings[1])}`,
-    );
+  console.log(
+    `cosine similarity: ${cosineSimilarity(embeddings[0]!, embeddings[1]!)}`,
+    `token used: ${usage.tokens}`,
+  );
 }
+
+void main();
